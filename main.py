d01c80547e894e9e8f337cf88130b2fb292ab86c
Tomas Garcia <tomasgarciadellacqua@gmail.com>
2023-03-01 16:17:12 -0300

    Entreno el modelo y lo evaluo

--- main.py
+++ main.py
@@ -69,3 +69,15 @@ X = vectorizer.transform(X)
 # splitting the dataset to training & test data
 X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2, stratify=Y, random_state=2)
 
+# training the model
+model = LogisticRegression()
+
+model.fit(X_train, Y_train)
+
+# evaluating the model
+# accuracy score
+X_train_prediction = model.predict(X_train)
+training_data_accuracy = accuracy_score(X_train_prediction, Y_train)
+
+print('Accuracy score of the training data: ', training_data_accuracy)
+
